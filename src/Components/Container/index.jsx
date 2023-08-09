f0624b6c04fa7915d8d82495e57ae5ef6687af02
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-08-09 17:54:09 -0300

    Fiz o tema 1

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -6,7 +6,7 @@ import Conta from "../Conta";
 import Extrato from "../Extrato";
 
 const Container = styled.div`
-   background-color: #f1f1f1;
+  background-color: ${({ theme }) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
 `
@@ -15,8 +15,7 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-
-  
+  color: ${({ theme }) => theme.text};
   @media (max-width: 800px) {
 
     flex-direction: column;
@@ -29,7 +28,7 @@ export default () => {
       <Titulo>Olá Fulano!</Titulo>
       < Conteudo>
         <Conta />
-        <Extrato/>
+        <Extrato />
       </ Conteudo>
     </Container>
   );
--- src/Components/GlobalStyle.js
+++ src/Components/GlobalStyle.js
@@ -8,7 +8,5 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   text-decoration: none;
-  color: grey;
 }
-
-`; 
+`;  
