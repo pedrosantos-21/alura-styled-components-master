699cdee0486457fcfa6108adbf9c5a242cf7d1a9
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-08-10 15:16:52 -0300

    Finalizei o Projeto🎉

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -5,7 +5,8 @@ export const conteudoClaro = "white";
 export const textoFundoClaro = "grey";
 
 export const fundoEscuro = "#363537";
-export const conteudoEscuro ="#5c5b5e";
+export const conteudoEscuro = "#5c5b5e";
 export const textoFundoEscuro = "FAFAFA";
+
 
         
